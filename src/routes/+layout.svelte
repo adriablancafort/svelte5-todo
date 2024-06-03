ac98e38fdbb7d085205d062323144fa4060b3e82
Adria Blancafort <adriablancafort@gmail.com>
2024-06-03 18:08:27 +0200

    todo app

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<svelte:head>
+	<title>Svelte 5 Todo App</title>
+</svelte:head>
+
+<main>
+	<slot />
+</main>
+
+<style>
+	:global(html, body) {
+		height: 100%;
+	}
+
+	:global(body) {
+		display: grid;
+		place-content: center;
+		background-color: #fff;
+	}
+</style>
